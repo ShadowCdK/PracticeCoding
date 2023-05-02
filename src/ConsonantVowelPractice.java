15de728c962ff5cf3480fc06c23578a32981251a
ShadowCdK <tirtaabayuu@gmail.com>
2023-05-02 19:34:47 +0700

    Make Looping for find Vocal and Consonant

--- src/ConsonantVowelPractice.java
+++ src/ConsonantVowelPractice.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ConsonantVowelPractice {
@@ -21,6 +22,28 @@ public class ConsonantVowelPractice {
         //Pembuatan Array huruf vokal dan konsonan
         char[] hurufVokal = ['a','i','u','e','o'];
         char[] hurufKonsonan = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+
+        //Perulangan untuk mencari jumlah huruf vokal dan konsonan
+        int jumlahHurufVokal = 0;
+        int jumlahHurufKonsonan = 0;
+
+        for (int i = 0; i<inputString.length(); i++) {
+            //Jumlah Huruf Konsonan
+            for (int j = 0; j < hurufKonsonan.length; j++) {
+                if(inputString.toLowerCase().charAt(i) == hurufKonsonan[j]){
+                    jumlahHurufKonsonan++;
+                }
+
+            }
+            //Jumlah Huruf Vokal
+            for (int j = 0; j < hurufVokal.length ; j++) {
+                if (inputString.toLowerCase().charAt(i) == hurufVokal[j]){
+                    jumlahHurufVokal++;
+                }
+
+            }
+        }
+
 
     }
 }
