a3c7d235613882d75175db31cc3a67538b02e30d
ShadowCdK <tirtaabayuu@gmail.com>
2023-05-02 18:40:48 +0700

    first commit

---
+++ src/ConsonantVowelPractice.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ConsonantVowelPractice {
+
+    public static void main(String[] args) {
+
+        //Method input (Scanner)
+        Scanner input = new Scanner(System.in);
+    }
+}
