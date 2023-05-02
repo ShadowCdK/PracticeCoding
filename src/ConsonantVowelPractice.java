92e993255430a07efa919a0024e7767beab710c1
ShadowCdK <tirtaabayuu@gmail.com>
2023-05-02 19:13:31 +0700

    Make Array for consonant vowel

--- src/ConsonantVowelPractice.java
+++ src/ConsonantVowelPractice.java
@@ -10,5 +10,17 @@ public class ConsonantVowelPractice {
         //Input
         System.out.println("Masukkan Teks : ");
         String inputString = input.nextLine();
+
+        //Proses
+        process(inputString);
+    }
+
+    //Method for process
+    private static void process (String inputString) {
+        //Proses
+        //Pembuatan Array huruf vokal dan konsonan
+        char[] hurufVokal = ['a','i','u','e','o'];
+        char[] hurufKonsonan = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
+
     }
 }
